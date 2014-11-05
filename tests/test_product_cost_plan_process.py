da2c3f9db65ed86d58a07e739a5949ad41464132
Sergi Almacellas Abellana <sergi@koolpi.com>
2014-11-05 09:43:25 +0100

    Migrate to version 3.4

--- tests/test_product_cost_plan_process.py
+++ tests/test_product_cost_plan_process.py
@@ -8,23 +8,17 @@ from trytond.tests.test_tryton import test_view, test_depends
 
 
 class TestCase(unittest.TestCase):
-    '''
-    Test module.
-    '''
+    'Test module'
 
     def setUp(self):
         trytond.tests.test_tryton.install_module('product_cost_plan_process')
 
     def test0005views(self):
-        '''
-        Test views.
-        '''
+        'Test views'
         test_view('product_cost_plan_process')
 
     def test0006depends(self):
-        '''
-        Test depends.
-        '''
+        'Test depends'
         test_depends()
 
 
