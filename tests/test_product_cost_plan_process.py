3c306ed888ee5ff5502b21283577a26c775eb275
Jes?s Mart?n Jim?nez <jesus@nan-tic.com>
2016-07-12 13:17:37 +0200

    Update test to version 4.0

--- tests/test_product_cost_plan_process.py
+++ tests/test_product_cost_plan_process.py
@@ -1,25 +1,14 @@
 #!/usr/bin/env python
 # The COPYRIGHT file at the top level of this repository contains the full
 # copyright notices and license terms.
-
 import unittest
 import trytond.tests.test_tryton
-from trytond.tests.test_tryton import test_view, test_depends
+from trytond.tests.test_tryton import ModuleTestCase
 
 
-class TestCase(unittest.TestCase):
+class TestCase(ModuleTestCase):
     'Test module'
-
-    def setUp(self):
-        trytond.tests.test_tryton.install_module('product_cost_plan_process')
-
-    def test0005views(self):
-        'Test views'
-        test_view('product_cost_plan_process')
-
-    def test0006depends(self):
-        'Test depends'
-        test_depends()
+    module = 'product_cost_plan_process'
 
 
 def suite():
